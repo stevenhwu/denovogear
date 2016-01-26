332157cf33960f053f22e567dd3f126091d5c9c7
Steven Wu <stevenhwu@gmail.com>
2016-01-26 02:22:14 -0700

    Update eigen functions with macro

--- src/include/dng/peeling.h
+++ src/include/dng/peeling.h
@@ -31,6 +31,9 @@
 #include <dng/matrix.h>
 #include <dng/detail/unit_test.h>
 #include <map>
+
+
+
 namespace dng {
 namespace peel {
 
@@ -41,6 +44,8 @@ enum {
     TOCHILDFAST,
     NUM // Total number of possible forward operations
 };
+
+
 
 std::map<decltype(peel::op::NUM), std::string> map_enum_string {
         {peel::op::UP, "UP"},
@@ -55,6 +60,9 @@ std::map<decltype(peel::op::NUM), std::string> map_enum_string {
         {peel::op::TOCHILDFAST, "TOCHILDFAST"}
 };
 } // namespace dng::peel::op
+
+#define WORKSPACE_T_MULTIPLE_UPPER_LOWER(workspace, index) \
+    (workspace.upper[index] * workspace.lower[index])
 
 struct workspace_t {
     IndividualVector upper; // Holds P(~Descendent_Data & G=g)
@@ -111,12 +119,20 @@ struct workspace_t {
         boost::copy(range, lower.begin() + library_nodes.first);
     }
 
+
     inline dng::GenotypeArray multiply_upper_lower(std::size_t index){
 
         return (upper[index] * lower[index]);
     }
+#define WORKSPACE_T_MULTIPLE_UPPER_LOWER(workspace, index) \
+    (workspace.upper[index] * workspace.lower[index])
+
+#define WORKSPACE_T_KRONECKER_PRODUCT_PARENTS(workspace, dad, mom) \
+    kroneckerProduct(WORKSPACE_T_MULTIPLE_UPPER_LOWER(work, dad).matrix(),  \
+                     WORKSPACE_T_MULTIPLE_UPPER_LOWER(work, mom).matrix() )
 
 };
+
 
 typedef std::vector<std::size_t> family_members_t;
 
@@ -136,6 +152,7 @@ dng::PairedGenotypeArray sum_over_children(workspace_t &work, const family_membe
 
 [[deprecated]] dng::GenotypeArray multiply_lower_upper(workspace_t &work, size_t index);
 
+[[deprecated]]
 dng::PairedGenotypeArray kroneckerProductDadMom(workspace_t &work,
                                                 std::size_t dad, std::size_t mom);
 
